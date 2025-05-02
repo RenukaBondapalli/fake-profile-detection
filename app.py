e78831b2f59ef3211696af0b49b86be1b3bd823b
Renuka Bondapalli <159697639+RenukaBondapalli@users.noreply.github.com>
2025-05-02 08:34:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -126,5 +127,8 @@ def index():
 
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
